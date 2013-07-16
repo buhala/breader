bc15c535c58c2b680747047daa0ad7586c5344ea
Hristo Dimitrov <me@buhala.uchenici.bg>
2013-07-16 10:58:12 +0300

    Javascript now can get the needed parameters
    I am too lazy to add the AJAX, though.

--- public/js/stories_loaded.js
+++ public/js/stories_loaded.js
@@ -4,6 +4,46 @@
  * This is a seperate file because it's nessesary for the AJAX to be completed before we do anything else.
  */
 $(function() {
+    $('.star').click(function(){
+       stripper=this.id.replace('story','').split('_');
+       parentId='#link'+stripper[0];
+       console.log(parentId);
+       link=$(parentId).attr('href');
+       /*
+                                        /@
+                       __        __   /\/
+                      /==\      /  \_/\/   
+                    /======\    \/\__ \__
+                  /==/\  /\==\    /\_|__ \
+               /==/    ||    \=\ / / / /_/
+             /=/    /\ || /\   \=\/ /     
+          /===/   /   \||/   \   \===\
+        /===/   /_________________ \===\
+     /====/   / |                /  \====\
+   /====/   /   |  _________    /  \   \===\    THE LEGEND OF 
+   /==/   /     | /   /  \ / / /  __________\_____      ______       ___
+  |===| /       |/   /____/ / /   \   _____ |\   /      \   _ \      \  \
+   \==\             /\   / / /     | |  /= \| | |        | | \ \     / _ \
+   \===\__    \    /  \ / / /   /  | | /===/  | |        | |  \ \   / / \ \
+     \==\ \    \\ /____/   /_\ //  | |_____/| | |        | |   | | / /___\ \
+     \===\ \   \\\\\\\/   /////// /|  _____ | | |        | |   | | |  ___  |
+       \==\/     \\\\/ / //////   \| |/==/ \| | |        | |   | | | /   \ |
+       \==\     _ \\/ / /////    _ | |==/     | |        | |  / /  | |   | |
+         \==\  / \ / / ///      /|\| |_____/| | |_____/| | |_/ /   | |   | |
+         \==\ /   / / /________/ |/_________|/_________|/_____/   /___\ /___\
+           \==\  /               | /==/
+           \=\  /________________|/=/    
+             \==\     _____     /==/ 
+            / \===\   \   /   /===/
+           / / /\===\  \_/  /===/
+          / / /   \====\ /====/
+         / / /      \===|===/
+         |/_/         \===/
+        */
+       rating=stripper[1]+1; //We want it to be an actual one, not a zero. We keep it a zero up to here, but now it becomes a one.
+       console.log(rating);
+    });
+        
     $('.star').mouseenter(function() {
         parent=$(this).attr('data-parent');
         i = this.id.split('_')[1];
